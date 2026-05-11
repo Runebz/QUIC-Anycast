832e630907bfd33ee8ab3a7a16659195386c23cb
Rune Ebbers <rune.ebbers@gmail.com>
2026-05-11 12:14:07 +0200

    socket setup pt. 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use mio::{Poll, Events, Token, Interest};
+use mio::net::UdpSocket;
+
+const MAX_DATAGRAM_SIZE = 1350;
+
 fn main() {
-    println!("Hello, world!");
+    let mut poll = Poll::new().unwrap();
+    let mut events = Events::with_capacity(1024);
+
+    let mut socket = UdpSocket::bind("127.0.0.1:4433".parse().unwrap()).unwrap();
+    poll.registry().register(&mut socket, Token(0), Interest::READABLE).unwrap();
+
+    let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION);
 }
+
